c5ec3fbc57f12e8508bd509c7233e0c834af6557
hazem <hazemk537@gmail.com>
2024-02-06 13:59:12 +0200

    v2 landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import LoginForm from './components/LoginForm';
 import MainLandingPage from './components/MainLandingPage';
 
 function App() {
   return (
     <div className="App">
-      <MainLandingPage/>
+      {/* <MainLandingPage/> */}
+      <LoginForm/>
     </div>
   );
 }
--- src/components/MainLandingPage.js
+++ src/components/MainLandingPage.js
@@ -1,35 +1,38 @@
 import React from "react";
-import NavBar from "./NavBar";
-
+import styles from "../styles/MainLandingpage.module.css";
+import Button from "../components/Button";
 function MainLandingPage() {
   return (
     <div>
-      <div className="landing-navbar">
-        <div className="logo">
-          <div className="landing-navbar-svg">
-            <svg
-              fill="#51a8bd"
-              viewBox="0 0 32 32"
-              xmlns="http://www.w3.org/2000/svg"
-              stroke="#51a8bd"
-            >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                <path d="m16 0c8.836556 0 16 7.163444 16 16s-7.163444 16-16 16-16-7.163444-16-16 7.163444-16 16-16zm1.0008661 2.0352252-.0008661 4.9647748h-2l.0001337-4.96484538c-6.9350811.48937684-12.47560224 6.02989798-12.96497908 12.96497908l4.96484538-.0001337v2l-4.9647748.0008661c.48983875 6.9346138 6.03016066 12.474626 12.9649085 12.9639793l-.0001337-4.9648454h2l.0008661 4.9647748c6.9342805-.4898152 12.4740935-6.0296282 12.9639087-12.9639087l-4.9647748-.0008661v-2l4.9648454.0001337c-.4893533-6.93474784-6.0293655-12.47506975-12.9639793-12.9649085zm-1.0008661 8.9647748c2.7614237 0 5 2.2385763 5 5s-2.2385763 5-5 5-5-2.2385763-5-5 2.2385763-5 5-5zm0 2c-1.6568542 0-3 1.3431458-3 3s1.3431458 3 3 3 3-1.3431458 3-3-1.3431458-3-3-3z"></path>
-              </g>
-            </svg>
-          </div>
-          <span className="landing-navbar-title"> Focus News </span>
+      <div className={styles.landingpage_body}>
+        <div className={styles.landingpage_navbar}>
+          <div className={styles.landingpage_logo}>
+            <div className="landing-navbar-svg">
+              <svg
+                fill="#51a8bd"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#51a8bd"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path d="m16 0c8.836556 0 16 7.163444 16 16s-7.163444 16-16 16-16-7.163444-16-16 7.163444-16 16-16zm1.0008661 2.0352252-.0008661 4.9647748h-2l.0001337-4.96484538c-6.9350811.48937684-12.47560224 6.02989798-12.96497908 12.96497908l4.96484538-.0001337v2l-4.9647748.0008661c.48983875 6.9346138 6.03016066 12.474626 12.9649085 12.9639793l-.0001337-4.9648454h2l.0008661 4.9647748c6.9342805-.4898152 12.4740935-6.0296282 12.9639087-12.9639087l-4.9647748-.0008661v-2l4.9648454.0001337c-.4893533-6.93474784-6.0293655-12.47506975-12.9639793-12.9649085zm-1.0008661 8.9647748c2.7614237 0 5 2.2385763 5 5s-2.2385763 5-5 5-5-2.2385763-5-5 2.2385763-5 5-5zm0 2c-1.6568542 0-3 1.3431458-3 3s1.3431458 3 3 3 3-1.3431458 3-3-1.3431458-3-3-3z"></path>
+                </g>
+              </svg>
+            </div>
+            <span className="landing-navbar-title"> Focus News </span>
+          </div >
+          <Button className="signin-btn" text="Sign In" />
+          <Button className="signup-btn" text="Sign Up" />
         </div>
-        <Button className="signin-btn" text="Sign In" />
-        <Button className="signup-btn" text="Sign Up" />
-
-        <header className="h1">Build your own newsfeed</header>
+        <h1 className={styles.landingpage_h1}>
+          Build your own newsfeed
+        </h1>
         <span className="description-1">
           With Focus, Follow your favorite websites , collect articles.
         </span>
@@ -42,7 +45,7 @@ function MainLandingPage() {
           screenshot of the app ان شاء الله
           <div className="features-landingpage">
             <div>
-              <img src="../../imagesRef/image-1.png"></img>
+              <img alt="error display " src="../../imagesRef/image-1.png"></img>
 
               <div className="header-h3">
                 Follow your favorite websites and creators
@@ -62,8 +65,8 @@ function MainLandingPage() {
               </span>
             </div>
             <div>
-              <img src="../../imagesRef/image-2.png" />
-
+              <img alt="error display " src="../../imagesRef/image-2.png" />
+              {/* reduedant names err */}
               <div className="header-h3">
                 smart algorithms and recommendation engines
               </div>
@@ -77,23 +80,23 @@ function MainLandingPage() {
               <span>Discover Based on your likes</span>
               <div>
                 <span>Recommended News</span>
-                <img />
+                <img alt="" />
               </div>
               <div>
                 <span>Tech News</span>
-                <img />
+                <img alt="" />
               </div>{" "}
               <div>
                 <span>Business & Finance</span>
-                <img />
+                <img alt="" />
               </div>{" "}
               <div>
                 <span>Marketing & Media </span>
-                <img />
+                <img alt="" />
               </div>
               <div>
                 <span>Hobby & Lifestyle</span>
-                <img />
+                <img alt="" />
               </div>
             </div>
           </div>
