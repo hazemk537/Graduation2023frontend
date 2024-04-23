c30a50b9be75bcc66a54bf3363a59b1c19335c33
mohamedz133 <mohammed31038890@f-eng.tanta.edu.eg>
2024-04-23 02:35:20 +0200

    home page modifications

--- src/routes/FeedView.js
+++ src/routes/FeedView.js
@@ -1,18 +1,11 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import '../styles/Gallary.css'
-import "../styles/homepage.css";
-import Profile from "../components/Profile";
+//import "../styles/homepage.css";
 import { useNavigate } from "react-router";
-import { Link } from "react-router-dom";
 import { useSearchParams } from "react-router-dom";
 function FeedView() {
   const navigate = useNavigate();
-  const user = {
-    name: "Mohamed Zaki",
-    image:
-      "http://1.gravatar.com/avatar/47db31bd2e0b161008607d84c74305b5?s=96&d=mm&r=g",
-    account: "mo.Zaki@gmail.com",
-  };
+
   const [userArticles, setUserArticles] = useState([]);
   useEffect(() => {
     fetch("https://dummyjson.com/products")
