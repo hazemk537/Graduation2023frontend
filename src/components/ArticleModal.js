d8a099e33f338b8753df2a4f8e81937ecdf451f5
mohamedz133 <mohammed31038890@f-eng.tanta.edu.eg>
2024-06-26 17:23:51 +0300

    Article model styling is done!!!!

--- src/components/ArticleModal.js
+++ src/components/ArticleModal.js
@@ -1,71 +1,50 @@
-import React from 'react'
-
+import React from 'react';
+import '../styles/SumWindow.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 function ArticleModal({ data, setArticleData }) {
-    // console.log(data)
     return (
         <>
-            <div
-                style={{
-
-                    width: '100vw',
-                    height: '100vh',
-                    position: 'fixed',
-                    zIndex: '400',
-                    backgroundColor:'red'
-            }}
-                className="receive_click_div_helper"
+            <div className="receive_click_div_helper"
                 onClick={() => {
                     console.log('clicked wrapper');
-                    setArticleData('')//close modal
-
+                    setArticleData(''); 
                 }}
             >
-
             </div>
 
-            <div
-                style={
-                    {
-                        backgroundColor: 'rgb(74, 67, 67)',
-                        width: '600px',
-                        height: '300px',
-                        zIndex:'500'
-                        , position: 'fixed',
-                        //-half width
-                       
-                        left: 'calc(50% - 300px )',
-                        //-half height
-                       
-                        top: 'calc(50% - 150px )',
-                        borderRadius: '20px'
-
-                    }
-                }
-
-            >
-
-                <p style={{
-
-
-                    fontSize: '20px'
-                    , lineHeight: '1.5',
-                    paddingLeft: '10rem',
-                    paddingTop: '2rem'
-
-
-                }}>
-                    {data.description}
-
-                </p>
-                <a target='_blank' rel="noreferrer" href={`${data.link}`}><svg style={{
-                    width: '4rem',
-                    height: '4rem',
-
-                }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><g fill="currentColor"><path d="M14.5 14.5v-3.25a.5.5 0 0 1 1 0V15a.5.5 0 0 1-.5.5H5a.5.5 0 0 1-.5-.5V5a.5.5 0 0 1 .5-.5h3.75a.5.5 0 0 1 0 1H5.5v9h9Z" /><path d="M10.354 10.354a.5.5 0 0 1-.708-.708l5-5a.5.5 0 0 1 .708.708l-5 5Z" /><path d="M15.5 8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 1 0v3.5Z" /><path d="M11.5 5.5a.5.5 0 0 1 0-1H15a.5.5 0 0 1 0 1h-3.5Z" /></g></svg></a>
-            </div >
+            <div className='sumwindow'>
+                <div className="close-button" 
+                    onClick={() => {
+                        console.log('clicked close button');
+                        setArticleData(''); 
+                    }}
+                >
+                    <FontAwesomeIcon icon={faTimes} id="X" />
+                </div>
+                
+                <div className="content">
+                    <p className='sum_p'>
+                        {data.description}
+                    </p>
+                    
+                    <button className='Full-Article-button'>
+                        <a target='_blank' rel="noreferrer" href={`${data.link}`}>
+                            <svg className='Full-Article' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <g fill="currentColor">
+                                    <path d="M14.5 14.5v-3.25a.5.5 0 0 1 1 0V15a.5.5 0 0 1-.5.5H5a.5.5 0 0 1-.5-.5V5a.5.5 0 0 1 .5-.5h3.75a.5.5 0 0 1 0 1H5.5v9h9Z" />
+                                    <path d="M10.354 10.354a.5.5 0 0 1-.708-.708l5-5a.5.5 0 0 1 .708.708l-5 5Z" />
+                                    <path d="M15.5 8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 1 0v3.5Z" />
+                                    <path d="M11.5 5.5a.5.5 0 0 1 0-1H15a.5.5 0 0 1 0 1h-3.5Z" />
+                                </g>
+                            </svg>
+                        </a>     
+                    </button>
+                </div>
+            </div> 
         </>
-    )
+    );
 }
 
-export default ArticleModal
+export default ArticleModal;
