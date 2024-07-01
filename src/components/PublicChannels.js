99aa02c7ab8d00385b7de4422ebd29b613b3b824
hazemk537 <hazemk537@gmail.com>
2024-07-01 13:43:05 +0300

    update

--- src/components/PublicChannels.js
+++ src/components/PublicChannels.js
@@ -45,6 +45,7 @@ function PublicChannels() {
 
     useEffect(() => {
         sendRequest(`https://gnews.io/api/v4/top-headlines?category=${categories[selectedSpan].key}&lang=${countries[selectedCountry].lang}&country=${countries[selectedCountry].key}&max=20&apikey=${apikey}`, { useEffect: true, method: 'Get', name: 'GnewsAPI', jsonFailProp: 'errors' });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [selectedSpan, selectedCountry]);
 
     let modifiedArticles = data.articles?.map((item, index) => ({
